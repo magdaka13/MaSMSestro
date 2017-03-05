b22c1c81a397e66ec9b0c5ad229997a5cee875a2
magda <magdalena.kazmierczak@wp.pl>
2017-03-05 02:06:46 +0100

    text changed

--- app/src/main/java/mg/masmsestro/MainActivity.java
+++ app/src/main/java/mg/masmsestro/MainActivity.java
@@ -18,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton NewSMS = (FloatingActionButton) findViewById(R.id. NewSMS);
+        NewSMS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, " New  SMS will be created here", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
