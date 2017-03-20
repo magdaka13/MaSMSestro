41e95b6a701ef6f9a0085e126e33bbe38815f105
magda <magdalena.kazmierczak@wp.pl>
2017-03-20 09:38:05 +0100

    updated DB structure and all DBhelper tables

--- app/src/main/java/mg/masmsestro/DBHelper.java
+++ app/src/main/java/mg/masmsestro/DBHelper.java
@@ -19,20 +19,9 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "SMSDB.db";
 
-
-    private SMS smsObj;
-    private Rule ruleObj;
-    private Settings settingsObj;
-    private SMSRefFolder smsRefFolderObj;
-
     public DBHelper(Context context)
      {
         super(context, DATABASE_NAME , null, 1);
-
-         ruleObj=new Rule();
-         settingsObj=new Settings();
-         smsRefFolderObj=new SMSRefFolder();
-
     }
 
     @Override
@@ -49,9 +38,16 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // TODO Auto-generated method stub
-        //db.execSQL("DROP TABLE IF EXISTS folders");
-        //onCreate(db);
+        db.execSQL("DROP TABLE IF EXISTS folders");
+        db.execSQL("DROP TABLE IF EXISTS sms");
+        db.execSQL("DROP TABLE IF EXISTS rule");
+        db.execSQL("DROP TABLE IF EXISTS settings");
+        db.execSQL("DROP TABLE IF EXISTS smsReffolder");
+
+        onCreate(db);
     }
+
+/* Folder table */
 
     public long insertFolder  (Folder f) {
 
@@ -110,6 +106,9 @@ public class DBHelper extends SQLiteOpenHelper {
         }
         return folder_list;
     }
+/* Folder table-end */
+
+/* SMS table */
 
     public long insertSMS  (SMS s) {
 
@@ -172,5 +171,205 @@ public class DBHelper extends SQLiteOpenHelper {
         }
         return sms_list;
     }
+/* SMS table-end */
+
+/* Rule table */
+
+    public long insertRule  (Rule r) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("rule",r.getRule());
+        contentValues.put("folder_id", r.getFolder_id());
+        long id=db.insert("rule", null, contentValues);
+
+        return id;
+    }
+
+    public Rule getRule(Rule r) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from rule where id_rule="+r.getId_rule()+"", null );
+
+        Rule ruleObj=new Rule();
+        ruleObj.setId_rule(res.getInt(res.getColumnIndex("id_rule")));
+        ruleObj.setRule(res.getString(res.getColumnIndex("rule")));
+        ruleObj.setFolder_id(res.getInt(res.getColumnIndex("folder_id")));
+
+        return ruleObj;
+    }
+
+    public int numberOfRowsRule(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, "rule");
+        return numRows;
+    }
+
+    public int updateRule(Rule r) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("rule", r.getRule());
+        contentValues.put("folder_id",r.getFolder_id());
+        return db.update("rule", contentValues, "id_rule = ? ", new String[] { Integer.toString(r.getId_rule()) } );
+
+    }
+
+    public Integer deleteRule (Rule r) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("rule","id_rule = ? ",new String[] { Integer.toString(r.getId_rule()) });
+    }
+
+    public List<Rule> getAllRule() {
+        List<Rule> rule_list = new ArrayList<Rule>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from rule", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            Rule r=new Rule();
+            r.setId_rule(res.getInt(res.getColumnIndex("id_rule")));
+            r.setRule(res.getString(res.getColumnIndex("rule")));
+            r.setFolder_id(res.getInt(res.getColumnIndex("folder_id")));
+            rule_list.add(r);
+            res.moveToNext();
+        }
+        return rule_list;
+    }
+/* Rule table-end */
+
+/* Settings table */
+
+    public long insertSetting  (Settings s) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id_setting",s.getId_setting());
+        contentValues.put("name", s.getName());
+        contentValues.put("value", s.getValue());
+
+        long id=db.insert("settings", null, contentValues);
+
+        return id;
+    }
+
+    public Settings getSetting(Settings s) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from settings where id_setting="+s.getId_setting()+"", null );
+
+        Settings settingsObj=new Settings();
+        settingsObj.setId_setting(res.getInt(res.getColumnIndex("id_setting")));
+        settingsObj.setName(res.getString(res.getColumnIndex("name")));
+        settingsObj.setValue(res.getString(res.getColumnIndex("value")));
+
+        return settingsObj;
+    }
+
+    public int numberOfRowsSettings(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, "settings");
+        return numRows;
+    }
+
+    public int updateSettings(Settings s) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", s.getName());
+        contentValues.put("value",s.getValue());
+        return db.update("rule", contentValues, "id_setting = ? ", new String[] { Integer.toString(s.getId_setting()) } );
+
+    }
+
+    public Integer deleteSetting (Settings s) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("settings","id_setting = ? ",new String[] { Integer.toString(s.getId_setting()) });
+    }
+
+    public List<Settings> getAllSettings() {
+        List<Settings> settings_list = new ArrayList<Settings>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from settings", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            Settings s=new Settings();
+            s.setId_setting(res.getInt(res.getColumnIndex("id_settings")));
+            s.setName(res.getString(res.getColumnIndex("name")));
+            s.setValue(res.getString(res.getColumnIndex("value")));
+            settings_list.add(s);
+            res.moveToNext();
+        }
+        return settings_list;
+    }
+/* Settings table-end */
+
+/* SMSrefFolder table */
+
+    public long insertSMSRefFolder  (SMSRefFolder s) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id_folder",s.getId_folder());
+        contentValues.put("id_SMS", s.getId_SMS());
+
+        long id=db.insert("SMSReffolder", null, contentValues);
+
+        return id;
+    }
+
+    public SMSRefFolder getSMSReffolder(SMSRefFolder s) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from SMSReffolder where id_ref="+s.getId_ref()+"", null );
+
+        SMSRefFolder smsRefFolderObj=new SMSRefFolder();
+        smsRefFolderObj.setId_ref(res.getInt(res.getColumnIndex("id_ref")));
+        smsRefFolderObj.setId_SMS(res.getInt(res.getColumnIndex("id_sms")));
+        smsRefFolderObj.setId_folder(res.getInt(res.getColumnIndex("id_folder")));
+
+        return smsRefFolderObj;
+    }
+
+    public int numberOfRowsSMSRefFolder(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, "SMSReffolder");
+        return numRows;
+    }
+
+    public int updateSMSRefFolder(SMSRefFolder s) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id_sms", s.getId_SMS());
+        contentValues.put("id_folder",s.getId_folder());
+        return db.update("SMSReffolder", contentValues, "id_ref = ? ", new String[] { Integer.toString(s.getId_ref()) } );
+
+    }
+
+    public Integer deleteSMSRefFolder (SMSRefFolder s) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("SMSReffolder","id_ref = ? ",new String[] { Integer.toString(s.getId_ref()) });
+    }
+
+    public List<SMSRefFolder> getAllSMSReffolder() {
+        List<SMSRefFolder> smsRefFolder_list = new ArrayList<SMSRefFolder>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from SMSReffolder", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            SMSRefFolder s=new SMSRefFolder();
+            s.setId_ref(res.getInt(res.getColumnIndex("id_ref")));
+            s.setId_SMS(res.getInt(res.getColumnIndex("id_sms")));
+            s.setId_folder(res.getInt(res.getColumnIndex("id_folder")));
+            smsRefFolder_list.add(s);
+            res.moveToNext();
+        }
+        return smsRefFolder_list;
+    }
+/* SMSRefFolder table-end */
+
 
 }
